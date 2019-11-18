a52f863f5fe983b3012b74a6e550136d72fdf0a7
Sebastian Liepe <dev@sebastianliepe.de>
2019-11-18 05:37:28 +0100

    Further work to release first version
    - Example extended with typescript watch compiling and file cleanup
    - Shortened call to shell-span by package.json bin option
    - Some cleanups

--- shell-spawn/shell-spawn.js
+++ shell-spawn/shell-spawn.js
@@ -6,15 +6,33 @@ var ShellSpawn = /** @class */ (function () {
     ShellSpawn.prototype.execute = function () {
         var child_process = require('child_process');
         var processArgv = process.argv;
+        // Needed spawn variables ---
         var spawnCommand = processArgv[2];
+        if (typeof spawnCommand === 'undefined') {
+            throw new Error('Command parameter missing!');
+        }
         var spawnParameters = processArgv.slice(3, processArgv.length);
         var spawnOptions = {
             detached: true,
             shell: true
         };
+        // ---
+        // We want to check, if a path separator is contained ---
+        var path = require('path');
+        var normalizedCommandPath = path.normalize(spawnCommand);
+        if (normalizedCommandPath.indexOf(path.sep) > -1) {
+            // Is it an relative path?
+            if (!path.isAbsolute(normalizedCommandPath)) {
+                var processCwd = process.cwd();
+                // Simply prepend current working dir of the node process
+                spawnCommand = processCwd + path.sep + spawnCommand;
+            }
+        }
+        // ---
+        // Spawn the process in a new shell
         var childProcess = child_process.spawn(spawnCommand, spawnParameters, spawnOptions);
         // https://nodejs.org/docs/latest-v12.x/api/child_process.html#child_process_child_process_spawn_command_args_options
-        /* not on detached
+        /* Not on detached
         childProcess.stdout.on('data', (data) => {
             console.log(`stdout: ${data}`);
         });
@@ -23,6 +41,10 @@ var ShellSpawn = /** @class */ (function () {
             console.error(`stderr: ${data}`);
         }); */
         childProcess.on('close', function (code) {
+            console.log("child process close all stdio with code " + code);
+        });
+        // https://nodejs.org/docs/latest-v12.x/api/child_process.html#child_process_event_close
+        childProcess.on('exit', function (code) {
             console.log("child process exited with code " + code);
         });
     };
