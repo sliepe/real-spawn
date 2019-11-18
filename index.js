55b8b10034aa29bd4049bba6c2a3cb818b086735
Sebastian Liepe <dev@sebastianliepe.de>
2019-11-18 13:29:11 +0100

    Cleanups/Fixed
    - Fixed/Cleaned example after renaming
    - Added "#! /usr/bin/env node" to index.ts, which accidentally was getting lost during previous tests
    - README.md

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var shell_spawn_js_1 = require("./shell-spawn/shell-spawn.js"); // We need tsconfig-paths then, if working with path`s compiler option
--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+#! /usr/bin/env node
+
 import ShellSpawn from './shell-spawn/shell-spawn.js'; // We need tsconfig-paths then, if working with path`s compiler option
 
 const shellSpawn = ShellSpawn.getInstance();
